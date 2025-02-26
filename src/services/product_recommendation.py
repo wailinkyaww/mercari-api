e4cb5d3f86b545797eec139ad84bd3a5cef2f303
Wai Lin Kyaw <wailink13@gmail.com>
2025-02-26 23:39:51 +0800

    improved prompt

--- src/services/product_recommendation.py
+++ src/services/product_recommendation.py
@@ -3,13 +3,13 @@ import json
 from openai import OpenAI
 from typing import List
 
+
 PRODUCT_RECOMMENDATION_PROMPT = """
 You are an expert at understanding user requests for online marketplaces, specifically for Mercari Japan. 
 
 You will be provided with the input from user and relevant product search results those are tailored to the user query.
 
 Your job is to pick top 3 most relevant products out of the provided product search results.
-
 You should also provide reasoning why each choice is relevant to user's request.
 
 **User Query**
@@ -18,7 +18,22 @@ You should also provide reasoning why each choice is relevant to user's request.
 **Relevant Products Found**
 {products}
 
-Now provide the output in presentable markdown format to user.
+
+Here is the outline for your response.
+
+#### 1. Product name
+- **Price:** Price with currency
+- **Reasoning:** Provide a reasoning description on why you recommend this product.
+<Markdown Image>
+<Link to see the details - use product url>
+
+#### 2. Product name
+- **Price:** Price with currency
+- **Reasoning:** Provide a reasoning description on why you recommend this product.
+<Markdown Image>
+<Link to see the details - use product url>
+
+Now provide the recommendation output up to 3 products in presentable markdown format to user.
 """
 
 
